4594f0b58cea7964c8c387c115990e0ba295f36c
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-06-14 22:31:07 -0600

    Commit 1 - Verbal questions.

---
+++ js/solutions.js
@@ -0,0 +1,23 @@
+// // ------------------------------------------------------------------------------
+// // Functions (Verbal Questions):
+// // ------------------------------------------------------------------------------
+
+// 1. Create a file called solutions.js. Write your answers in this file.
+	// Done.
+
+// 2.  What is the difference between a parameter and an argument?
+
+	// Answer:
+	// A parameter is a variable in a method definition. 
+	// When a method is called, the arguments are the data you pass into the method's parameters. 
+	// Parameter is variable in the declaration of function. 
+	// Argument is the actual value of this variable that gets passed to function.
+
+// 3.  What is the difference between a parameter and an argument?
+	// Answer:
+	// console.log() outputs a "log" in the console and is only meant for the 
+	// user to see and check code. It also exports in string-format.
+	// return is a command that belongs-to, and only works within functions. 
+	// return exports data from a function to the outside world. return exports data
+	// in it's raw format.
+
